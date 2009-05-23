b5a073e444d15fe8a7c16fa361720f7e82a71ca6
mo_khan <mo@mokhan.ca>
2009-05-22 20:55:18 -0600

    nasa is now deploying rovers to starting coordinates

--- product/project/NASA.cs
+++ product/project/NASA.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace mars.rover
 {
     public class NASA
     {
         public virtual void report_top_left_coordinates_to(SpecifyTopLeftCoordinates callback)
         {
+        }
+
+        public virtual Rover deploy_rover_to(int x_coordinate, int y_coordinate, Heading heading)
+        {
+            return new Rover(new Position(x_coordinate,y_coordinate,heading));
         }
     }
 }
